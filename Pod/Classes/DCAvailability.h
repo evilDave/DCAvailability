0e45826ed8425c636ba2dd495290b34f1a10c718
David Clark <david.clark@hotelscombined.com>
2016-11-18 09:31:52 +1100

    Support iOS 10.1

--- Pod/Classes/DCAvailability.h
+++ Pod/Classes/DCAvailability.h
@@ -53,6 +53,7 @@
 #define DC_AVAILABILITY_9_2(_ios) CF_AVAILABLE_IOS(_ios)
 #define DC_AVAILABILITY_9_3(_ios) CF_AVAILABLE_IOS(_ios)
 #define DC_AVAILABILITY_10_0(_ios) CF_AVAILABLE_IOS(_ios)
+#define DC_AVAILABILITY_10_1(_ios) CF_AVAILABLE_IOS(_ios)
 
 // Macro to force the deprecated message, do the weak import, when needed
 #define DC_DEPRECATED_NEWAPI(_ios) __attribute__((deprecated("Available starting API " #_ios ", which is newer than the Deployment Target"), weak_import))
@@ -252,6 +253,13 @@
 #define __AVAILABILITY_INTERNAL__IPHONE_10_0 DC_DEPRECATED_NEWAPI(10_0)
 #undef DC_AVAILABILITY_10_0
 #define DC_AVAILABILITY_10_0(_ios) DC_DEPRECATED_NEWAPI(_ios)
+#endif
+
+#if __IPHONE_10_1 > __IPHONE_OS_VERSION_MIN_REQUIRED & __IPHONE_10_1 <= __IPHONE_OS_VERSION_MAX_ALLOWED
+#undef  __AVAILABILITY_INTERNAL__IPHONE_10_1
+#define __AVAILABILITY_INTERNAL__IPHONE_10_1 DC_DEPRECATED_NEWAPI(10_1)
+#undef DC_AVAILABILITY_10_1
+#define DC_AVAILABILITY_10_1(_ios) DC_DEPRECATED_NEWAPI(_ios)
 #endif
 
 // Reroute usages of the NS_AVAILABLE_IOS macro to our own version specific ones (possibly the default, possibly redefined)
